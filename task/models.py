56c6c12b21c4fa262f97cc5c30d63c3021b8ed6f
jgrynczewski <jgrynczewski@gmail.com>
2023-10-08 12:01:37 +0200

    add task model + migration

--- task/models.py
+++ task/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
+
 # Create your models here.
+class Task(models.Model):
+    name = models.CharField(max_length=50)
+    description = models.TextField()
+    deadline = models.DateTimeField()
+    status = models.CharField(max_length=32)
+
+    added = models.DateTimeField(auto_now_add=True)
