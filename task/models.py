f2a3c591a8961299dbff34ffbe92573104673cf2
jgrynczewski <jgrynczewski@gmail.com>
2023-10-08 12:40:48 +0200

    register task model in the admin panel

--- task/models.py
+++ task/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 # Create your models here.
 class Task(models.Model):
-    name = models.CharField(max_length=50)
+    name = models.CharField(max_length=50, blank=True)
     description = models.TextField()
     deadline = models.DateTimeField()
     status = models.CharField(max_length=32)
